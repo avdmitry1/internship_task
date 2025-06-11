7b0acb551549211fb63283fbda032feb874e79f0
avdmitry1 <avdmitry1@gmail.com>
2025-06-11 18:56:28 +0300

    Enhance table name pluralization using inflect library
    Adds alembic and inflect as new project dependencies for database migrations and word inflection support.

--- fastapi-app/core/models/base.py
+++ fastapi-app/core/models/base.py
@@ -1,11 +1,14 @@
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, declared_attr
+import inflect
+
+inflect_engine = inflect.engine()
 
 
 class Base(DeclarativeBase):
     __abstract__ = True
 
-    @declared_attr.directive
+    @declared_attr
     def __tablename__(cls):
-        return f"{cls.__name__.lower()}s"
+        return inflect_engine.plural(cls.__name__.lower())
 
     id: Mapped[int] = mapped_column(primary_key=True)
