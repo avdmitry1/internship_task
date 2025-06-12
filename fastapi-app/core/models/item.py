b1eb0e567ca40d28fc383600fb96e5b645bf7b12
avdmitry1 <avdmitry1@gmail.com>
2025-06-12 09:50:50 +0300

    rename model

---
+++ fastapi-app/core/models/item.py
@@ -0,0 +1,8 @@
+from .base import Base
+from sqlalchemy.orm import Mapped, mapped_column
+
+
+class Item(Base):
+    item: Mapped[str] = mapped_column(unique=True)
+    description: Mapped[str] = mapped_column()
+    remaked_description: Mapped[str] = mapped_column()
